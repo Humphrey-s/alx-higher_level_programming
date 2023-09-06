0ff9a81ac68c139ef1664b078d3901a4592cfc11
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-06 21:47:59 +0300

    combbo2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-num = "{l1}"
-num2 = "{b}, "
 for n in range(0, 100):
-    if n < 10:
-        print(num.format(l1 = 0), end= '')
-
-    print(num2.format(b = n), end= '')
+    if n == 99:
+        print("{}".format(n))
+    else:
+        print("{:02}".format(n), end= ', ')
