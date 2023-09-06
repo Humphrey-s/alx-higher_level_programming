c066bf7c5d3fec81b2ae8a582717b2cc76c9672a
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-06 21:36:12 +0300

    numbers in combo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+num = "{l1}"
+num2 = "{b}, "
 for n in range(0, 100):
     if n < 10:
-        print(f"0", end= '')
+        print(num.format(l1 = 0), end= '')
 
-    print(f"{n},", end= ' ')
+    print(num2.format(b = n), end= '')
