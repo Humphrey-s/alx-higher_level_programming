0b9906c08eeb9bc00a5477afad655472d89f5062
Greybeard <greybeard@gmail.com>
2023-09-08 09:16:16 +0300

    fizzbuzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -8,6 +8,8 @@ def fizzbuzz():
             print("FizzBuzz ".format(), end= '')
         elif i % 5 == 0:
             print("Buzz ".format(), end= '')
+        elif i % 3 == 0:
+            print("Fizz ".format(), end= '')
         else:
             print("{} ".format(i), end= '')
 
