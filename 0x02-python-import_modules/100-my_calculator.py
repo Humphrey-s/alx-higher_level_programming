24622bbdcae290d89ceaa4bfcc52317de96bd3d5
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-08 08:28:37 +0300

    calc 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,8 @@ def main():
     ln = len(sys.argv)
 
     if ln == 1 or ln == 2 or ln == 3 or ln > 4:
-        print("0".format())
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>".format())
+        sys.exit(1)
     else:
         op = str(sys.argv[2])
         a = int(sys.argv[1])
@@ -25,6 +26,7 @@ def main():
                 print("{} / {} = {}".format(a, b, div(a, b)))
             case _:
                 print("Unknown operator. Available operators: +, -, * and /".format())
+                sys.exit(1)
 
 if __name__ == "__main__":
     main()
