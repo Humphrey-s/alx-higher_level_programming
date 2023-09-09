2c7eea7f6ea6a9c46c418de8664b3c77b6f9cb6d
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-09 20:22:00 +0300

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,8 @@ from calculator_1 import add, sub, mul, div
 
 
 def main():
-    a, b = 10, 5
+    a = 10
+    b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
