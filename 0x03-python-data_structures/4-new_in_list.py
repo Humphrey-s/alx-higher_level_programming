fa8d299fd2f987067e820d7947db2a21deda9998
Greybeard <greybeard@gmail.com>
2023-09-11 08:01:56 +0300

    New list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    n = len(my_list)
+
+    if idx < 0 or idx > n:
+
+        return my_list.copy()
+
+    else:
+
+        list_cp = my_list.copy()
+
+        list_cp[idx] = element
+
+        return list_cp
