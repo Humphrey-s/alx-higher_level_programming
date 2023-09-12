d349e8e686ca11f68594ad413b219125ca8eefda
Greybeard <greybeard@gmail.com>
2023-09-12 09:02:46 +0300

    max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -16,3 +16,18 @@ def max_integer(my_list=[]):
     elif n == 1:
 
         return my_list[0]
+
+    else:
+
+        for i in range(n):
+
+            if max_n[0] < my_list[i]:
+
+                max_n[0] = my_list[i]
+
+            elif max_n[0] >= my_list[i]:
+
+                max_n[0] = max_n[0]
+
+        return max_n[0]
+
