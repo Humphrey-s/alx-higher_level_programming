6ce97d178b6c046db96f429feab965e3c6d48274
Greybeard <greybeard@gmail.com>
2023-09-12 21:29:51 +0300

    element && index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    
+
     n = len(my_list)
 
     if idx > n - 1 or idx < 0:
 
         return None
 
-    elif idx == n:
-
+    elif idx == n - 1:
 
         return my_list[n - 1]
     else:
