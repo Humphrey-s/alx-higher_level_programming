46d09edc5a4bd162a3fa30c3f991a07142929970
Greybeard <greybeard@gmail.com>
2023-09-13 22:59:25 +0300

    sq matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,12 +2,19 @@
 
 def square_matrix_simple(matrix=[]):
 
+    if matrix == None:
+
+        return None
+
     n = len(matrix)
+
+    if n == 0:
+
+        return matrix
 
     new_matrix = []
 
     new_matrix = list(matrix)
-
 
     for i in range(n):
 
