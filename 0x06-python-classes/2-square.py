6dd7bfa9935600ece8a41ff91b2d72d5c733ff37
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-26 22:44:28 +0300

    error message

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Building a Class"""
+
+class Square:
+    """Defining a Class Square"""
+
+
+    def __init__(self, size=0):
+        """Initializing
+
+        Args:
+            size: size of square
+
+        """
+
+        if isinstance(size, int):
+
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
