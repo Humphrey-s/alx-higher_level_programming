b728e2b0505355642035ab1ec9ab2784510b9e99
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-26 23:07:30 +0300

    getting are of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Representation of a Class."""
+
+class Square:
+    """Defining Square."""
+
+    def __init__(self, size=0):
+        """initializing Square
+
+        Args:
+            size (int): size of square.
+        """
+        if isinstance(size, int):
+
+            if size < 0:
+
+                raise ValueError("size must be >= 0")
+            else:
+
+                self.__size = size
+
+        else:
+            raise TypeError("size must be an integer")
+    
+    def area(self):
+
+        a = self.__size
+
+        return a * a
