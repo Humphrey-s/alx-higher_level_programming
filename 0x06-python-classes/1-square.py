3224d538ee42147554b43c1cf1d91b81a732af07
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-26 22:02:52 +0300

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining class Square."""
+
+
+class Square:
+    """Initializing the Class"""
+
+    def __init__(self, size):
+        """initializing the class
+
+        Args:
+             
+             size (int): square size
+        """
+        self.__size = size
+
