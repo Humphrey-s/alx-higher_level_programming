e080894404cb7b4309bfb092bfc2ad8cafa87a73
Greybeard <odhiambohumphrey78@gmail.com>
2023-09-27 22:18:34 +0300

    100-safe

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+
+    try:
+
+        print("{:d}".format(value))
+        return True
+
+    except ValueError as e:
+
+        print("Exception: {}".format(e))
+        pass
+        return False
+    except Exception as e:
+
+        print("Exception: {}".format(e))
+        pass
+        return False
