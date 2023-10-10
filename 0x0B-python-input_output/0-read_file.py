037236342ec41d6065f61f0fbc09ff1742628ad5
Greybeard <odhiambohumphrey78@gmail.com>
2023-10-10 23:10:03 +0300

    filename, print(f.read())

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,8 +2,6 @@
 
 def read_file(filename=""):
 
-    with open(str(filename), encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
 
-        for line in f:
-
-            print(line, end="")
+        print(f.read(), end="")
