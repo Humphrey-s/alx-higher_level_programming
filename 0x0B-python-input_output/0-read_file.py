9269bc885f00ac390e96b1f346b8c4af6d90273d
Greybeard <odhiambohumphrey78@gmail.com>
2023-10-10 23:13:36 +0300

    reads file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/pthon3
+"""Defining read_file function."""
+
 
 def read_file(filename=""):
+    """Reads a file content in utf-8 encoding."""
 
     with open(filename, encoding="utf-8") as f:
 
