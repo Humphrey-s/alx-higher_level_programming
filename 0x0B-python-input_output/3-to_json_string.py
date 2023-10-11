ccf050774207e2d2d1b78a8230f721055bf857ee
Greybeard <odhiambohumphrey78@gmail.com>
2023-10-11 08:30:59 +0300

    to json- doc

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import json
-"""Define to_json_string."""
 
 
 def to_json_string(my_obj):
-    """Converts obj to json rep of a string."""
-    try:
-        return json.dumps(my_obj)
-    except:
-        p = json.dumps(my_obj)
 
-        return p
+    return json.dumps(my_obj)
