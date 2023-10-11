3551a127a3101f6c8e9fb612eb16cd6696761e29
Greybeard <odhiambohumphrey78@gmail.com>
2023-10-11 08:57:18 +0300

    3 is documented

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 import json
+"""Define to_json_string."""
 
 
 def to_json_string(my_obj):
+    """Returns json rep of py data
+
+    Args:
+       my_obj (str): object
+    Returns:
+       json representation
+    """
 
     return json.dumps(my_obj)
