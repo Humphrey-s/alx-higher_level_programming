dc79f0b9a38e2f24e8747cda49fe60d654a43427
greybeard <odhiambohumphrey78@gmail.com>
2023-10-15 21:46:26 +0300

    subclass

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
 
         if type(value) != int:
 
-            raise TypeError("{} must be an integer".fomat(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
 
             raise ValueError("{} must be greater than 0".format(name))
