f6f0f66e1b4eb672ffbb195961a3e499498f78df
greybeard <odhiambohumphrey78@gmail.com>
2023-10-17 05:11:56 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""Class Square."""
+"""Square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Definition Square subclass."""
+    """Geometrical square subclass."""
 
     def __init__(self, size, x=0, y=0, id=None):
 
         super().__init__(size, size, x, y, id)
-
-    @property
-    def size(self):
-
-        return self.width
-
-    @size.setter
-    def size(self, size):
-
-        self.width = size
-        self.height = size
