fa278426b3936c362708ab02da3cec664f76820d
greybeard <odhiambohumphrey78@gmail.com>
2023-12-09 19:49:13 +0300

    None

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -5,18 +5,22 @@
 def print_square(size):
 
     try:
+
+        if size is None:
+            raise TypeError("size must be an integer")
+
         if isinstance(size, float) and size < 0:
             raise TypeError("size must be an integer")
 
-        if not isinstance(size, int) or size is None:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
 
         if size < 0:
             raise ValueError("size must be >= 0")
-        
+
         if size == 0:
             pass
-        
+
         else:
             square = ""
             w = 0
