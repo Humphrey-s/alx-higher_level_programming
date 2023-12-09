9c03bdc0996af91bb034bd87c721529a7b1e8a3f
greybeard <odhiambohumphrey78@gmail.com>
2023-12-09 19:18:43 +0300

    empty arguments

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -6,6 +6,9 @@ def say_my_name(first_name, last_name=""):
     """prints My name is <first name> <last name>"""
 
     if isinstance(first_name, str) and isinstance(last_name, str):
+
+        if len(first_name) == 0 and len(last_name) == 0:
+            pass
         print("My name is {} {}".format(first_name, last_name))
 
     elif not isinstance(first_name, str):
