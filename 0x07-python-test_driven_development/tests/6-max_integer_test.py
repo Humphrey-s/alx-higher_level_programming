e5188e76ea4e311b41f2e7637561aa8be9c6ae7e
greybeard <odhiambohumphrey78@gmail.com>
2023-12-09 23:59:37 +0300

    unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,9 +11,13 @@ class TestMaxInteger(unittest.TestCase):
 
         maxx = max_integer([20, 98, 67, 90])
         maxx2 = max_integer([-1, -89, -1000, 0])
+        maxx3 = max_integer([1])
+        max4 = max_integer([1000, 200, 19])
 
         self.assertEqual(maxx2, 0)
         self.assertEqual(maxx, 98)
+        self.assertEqual(maxx3, 1)
+        self.assertEqual(max4, 1000)
 
     def test_Elist(self):
 
