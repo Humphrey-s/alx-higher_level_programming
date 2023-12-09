89da5ece9e9881ba60bfc4586aebff7c48c73a42
greybeard <odhiambohumphrey78@gmail.com>
2023-12-09 20:18:08 +0300

    tests

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -3,36 +3,30 @@
 
 
 def print_square(size):
+    """prints square"""
 
-    try:
-
-        if size is None:
-            raise TypeError("size must be an integer")
-
-        if isinstance(size, float) and size < 0:
-            raise TypeError("size must be an integer")
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
-        if size == 0:
-            pass
-
-        else:
-            square = ""
-            w = 0
-
-            for h in range(0, size):
-
-                while w < size:
-                    square += "#"
-                    w = w + 1
-                w = 0
-                if h + 1 != size:
-                    square += "\n"
-            print(square)
-    except Exception as e:
+    if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
+
+    if not isinstance(size, int) or size is None:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if size == 0:
+        pass
+
+    else:
+        square = ""
+        w = 0
+
+        for h in range(0, size):
+
+            while w < size:
+                square += "#"
+                w = w + 1
+            w = 0
+            if h + 1 != size:
+                square += "\n"
+        print(square)
