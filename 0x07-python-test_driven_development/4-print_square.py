8b34a2f51d210a27b521d2786334ce0d5a0b130f
greybeard <odhiambohumphrey78@gmail.com>
2023-12-09 19:33:57 +0300

    size == NONE

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@ def print_square(size):
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
-    if not isinstance(size, int):
+    if not isinstance(size, int) or size is None:
         raise TypeError("size must be an integer")
 
     if size < 0:
