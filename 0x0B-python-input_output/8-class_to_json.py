30bb1afb53027321fdbfe2899cecf8b50ce57484
greybeard <odhiambohumphrey78@gmail.com>
2023-12-31 02:13:03 +0300

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """definition class_to_json"""
-import json
 
 
 def class_to_json(obj):
     """convert class to json"""
 
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
