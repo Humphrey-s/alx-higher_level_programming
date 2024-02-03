330cf50d1aea617bbcd34885a6e6c96f84faaa79
greybeard <greybeard@gmail.com>
2024-02-03 23:10:56 +0300

    3

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE hbtn_0d_2;
 USE hbtn_0d_2;
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
