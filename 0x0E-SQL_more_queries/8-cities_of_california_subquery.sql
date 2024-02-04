6609e58f48da6bc15f54f04f9aaf511206f96d06
greybeard <greybeard@gmail.com>
2024-02-04 11:48:43 +0300

    state_id = 1

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa --
-SELECT id, state_id, name FROM hbtn_0d_usa.cities ORDER BY id ASC;
+SELECT id, name FROM hbtn_0d_usa.cities WHERE state_id = 1 ORDER BY id ASC;
