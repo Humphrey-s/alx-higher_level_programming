c86ed45b3ad35c023288ae6c71e76da652fb436e
greybeard <greybeard@gmail.com>
2024-02-04 17:55:59 +0300

    genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked --
+SELECT tv_genres.name as genre, COUNT(tv_genres.id) as number_of_shows  FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name, tv_genres.id ORDER BY number_of_shows DESC;
