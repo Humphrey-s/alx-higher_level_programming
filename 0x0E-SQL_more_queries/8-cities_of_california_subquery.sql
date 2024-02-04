7f54cc5d78b85cbe679cf291146e154129e10aeb
greybeard <greybeard@gmail.com>
2024-02-04 11:23:32 +0300

    initial commit

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa --
-SELECT id FROM cities ORDER BY (ASC);
+SELECT id, state_id, name FROM hbtn_0d_usa.cities ORDER BY id ASC;
