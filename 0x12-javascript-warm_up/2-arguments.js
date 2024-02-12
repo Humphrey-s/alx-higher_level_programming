d5f029ee4a43c5e4c1ab21be4d4086f6a9bb35a6
greybeard <greybeard@gmail.com>
2024-02-12 19:38:42 +0300

    last check

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -14,7 +14,7 @@ while (it <= 5) {
 if (it === 3) {
   console.log('Argument found');
 } else if (it > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 } else {
   console.log('No Argument');
 }
