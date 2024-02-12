6b7505f0df0f749a46790390b47f22810242a4f2
greybeard <greybeard@gmail.com>
2024-02-12 19:28:17 +0300

    initial commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv;
+let it = 0;
+
+while (it <= 5) {
+  if (!args[it]) {
+    break;
+  }
+
+  it = it + 1;
+}
+
+if (it === 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No Argument');
+}
