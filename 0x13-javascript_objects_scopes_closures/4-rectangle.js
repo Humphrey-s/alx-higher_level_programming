bf4c987c263c428cd7b3b36ed4608913c2260c96
greybeard <greybeard@gmail.com>
2024-02-13 18:30:07 +0300

    initial commit

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node.js
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  rotate () {
+    const w = this.width;
+    this.width = this.height;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+
+  print () {
+    let h = 0;
+    let w;
+    let rectangle;
+
+    while (h < this.height) {
+      w = 0;
+      rectangle = '';
+      while (w < this.width) {
+        rectangle = rectangle + 'X';
+        w = w + 1;
+      }
+      console.log(rectangle);
+      h = h + 1;
+    }
+  }
+}
+
+module.exports = Rectangle;
