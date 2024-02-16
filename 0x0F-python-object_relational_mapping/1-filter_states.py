964a11db45fc8e4d9254b877b7246df37d8f2f6b
greybeard <greybeard@gmail.com>
2024-02-16 18:01:30 +0300

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
@@ -8,12 +9,15 @@ def main():
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    dbase =MySQLdb.connect(host="localhost", port=3306, user=username, password=password, database=db_name, charset="utf8")
+    try:
+        dbase =MySQLdb.connect(host="localhost", port=3306, user=username, password=password, database=db_name, charset="utf8")
+    except Exception:
+        print("connection failed")
 
     cursor = dbase.cursor()
 
-    cursor.execute("SELECT states.id, states.name FROM {}.states WHERE states.id > 3 ORDER BY states.id ASC".format(db_name))
-    
+    cursor.execute("SELECT * FROM {}.states WHERE states.id > 3 ORDER BY states.id ASC;".format(db_name))
+
     rows = cursor.fetchall()
 
     for row in rows:
