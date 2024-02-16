69dce0ccbae40bcf383df9bff1d74a5e8011d917
greybeard <greybeard@gmail.com>
2024-02-16 18:04:21 +0300

    all

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 #script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-import MySQLdb
-import sys
 
 def main():
     import MySQLdb
