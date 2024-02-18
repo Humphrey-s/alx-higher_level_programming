671e830b4aa96c6529f03fdb49d16fc10d640157
greybeard <greybeard@gmail.com>
2024-02-18 13:06:15 +0300

    not import

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,13 +7,17 @@ from model_state import Base, State
 from sqlalchemy.orm import Session
 import sys
 
-engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
+def main():
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-with Session(engine) as session, session.begin():
 
-    statement = select(State).order_by(State.id)
+    with Session(engine) as session, session.begin():
+        statement = select(State).order_by(State.id)
 
-    result = session.execute(statement)
+        result = session.execute(statement)
 
-    for obj in result.scalars():
-        print("{}: {}".format(obj.id, obj.name))
+        for obj in result.scalars():
+            print("{}: {}".format(obj.id, obj.name))
+
+if __name__ == "__main__":
+    main()
