cd1b0ed462027c2013807febc18ef86dc6d2c6e0
greybeard <greybeard@gmail.com>
2024-02-18 11:31:09 +0300

    states comment

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,10 +7,13 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 import MySQLdb as _mysql
 
+
 Base = declarative_base()
 
 class State(Base):
-
+    """
+    State table class
+    """
     __tablename__ = "states"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(288), nullable=False)
