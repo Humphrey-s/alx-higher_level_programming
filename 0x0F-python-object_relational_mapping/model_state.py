e1cc14d9fbad4263023cdcdd096432ceb7d33ed5
greybeard <greybeard@gmail.com>
2024-02-18 10:46:25 +0300

    initial commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ a python file that contains the class definition of a State and an instance Base = declarative_base()
+"""
+import sys
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+import MySQLdb as _mysql
+
+Base = declarative_base()
+
+class State(Base):
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(288), nullable=False)
