d3a737f6a5ed3d0e626ca2266a1f0d3c4271fb5b
greybeard <greybeard@gmail.com>
2024-02-29 21:48:31 +0300

    3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-if [ "$#" -ne 1 ];
-then
-	exit;
-fi
-
-size=$(curl -s $1 | wc -c)
-
-echo "$size"
+echo $(curl -s $1 | wc -c)
