bbbd46432999ac226a7f385d87d1b571b5688af6
greybeard <greybeard@gmail.com>
2024-03-01 19:14:41 +0300

    halloo

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,10 @@ def main():
 
     with request.urlopen("https://alx-intranet.hbtn.io/status") as r:
         print("Body response:")
+        content = r.read().decode("utf-8")
         print("\t- type: ".format(type(r.read())))
-        content = r.read()
-        print("\t- content: {}", content)
+        print("\t- content: {}".format(r.read()))
+        print("\t- utf-8 content: {}".format(content))
 
 if __name__ == "__main__":
     main()
