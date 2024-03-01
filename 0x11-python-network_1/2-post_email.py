6c9d7a108292a15449d797d850e96a6715fd88c0
greybeard <greybeard@gmail.com>
2024-03-01 18:28:49 +0300

    haa

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)"""
+import sys
+import urllib
+from urllib import request
+
+def main():
+
+    values = {"email": sys.argv[2]}
+
+    data = urllib.parse.urlencode(values).encode("ascii")
+    url = request.Request(sys.argv[1], data)
+
+    with request.urlopen(url) as r:
+        print(r.read)
+
+if __name__ == "__main__":
+    main()
