59281d37e3433efe4dc46221b29076965dec3e77
greybeard <greybeard@gmail.com>
2024-03-01 18:01:00 +0300

    url

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in
+from urllib import request
+import sys
+
+def main():
+    args = sys.argv[1]
+
+    with request.urlopen(args) as r:
+        print(r.headers["X-Request-Id"])
+
+if __name__ == "__main__":
+    main()
