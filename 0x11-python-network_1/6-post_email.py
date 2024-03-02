2c5c0b380f4f9415783cc51860ebdbdaa40b628f
greybeard <greybeard@gmail.com>
2024-03-02 23:04:44 +0300

    post requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,5 @@ import sys
 import requests
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1], data={"email": sys.argv[2]})
-    print(r.text)
+    r = requests.post(sys.argv[1], data={"email": sys.argv[2]})
+    print(r.content)
