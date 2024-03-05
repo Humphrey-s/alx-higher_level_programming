20c76cbef3dca64a687f9abbb9279f6349215a6f
greybeard <greybeard@gmail.com>
2024-03-05 14:14:30 +0300

    l

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,12 +8,8 @@ from urllib import request
 if __name__ == "__main__":
 
     values = {"email": sys.argv[2]}
-
     data = urllib.parse.urlencode(values).encode("ascii")
     url = request.Request(sys.argv[1], data)
 
     with request.urlopen(url) as r:
         print(r.read().decode("utf-8"))
-
-if __name__ == "__main__":
-    main()
