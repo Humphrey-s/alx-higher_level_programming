e48244946a906a06fe23d7c47ffd1ade00e8583a
greybeard <greybeard@gmail.com>
2024-03-05 13:10:20 +0300

    hall

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -s -X PUT -d "You got me!" 0.0.0.0:5000/catch_me
+curl -sL -X PUT --data-raw "You got me!" 0.0.0.0:5000/catch_me
