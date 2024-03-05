9311e4fc274b5e8339be69f3a04e1ae21293e12f
greybeard <greybeard@gmail.com>
2024-03-05 12:30:46 +0300

    server: You got me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message
-curl -s -X POST -d "body=You got me!" $1
+curl -sL -X PUT -d "body=You got me!" 0.0.0.0:5000/catch_me
