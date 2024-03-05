1be6b8883f3c38e6cc8bf1626e67611970a35c90
greybeard <greybeard@gmail.com>
2024-03-05 15:24:01 +0300

    l

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,14 +2,17 @@
 """ Takes in a URL, sends a request to the URL and displays response"""
 import sys
 from urllib import request
+import urllib.error
 
 
 if __name__ == "__main__":
 
     url = request.Request(sys.argv[1])
+
     with request.urlopen(url) as r:
         try:
-            print(r.read().decode("utf-8"))
+            print(r.read().decode("ascii"))
+
         except urllib.error.HTTPError as e:
             p = e.code
             print("Error code: {}".format(p))
