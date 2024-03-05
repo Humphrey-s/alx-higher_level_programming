0ff1801decdcb0877874cd2fb0e0dc77a31737c6
greybeard <greybeard@gmail.com>
2024-03-05 10:09:45 +0300

    shh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -i -sL -X OPTIONS $1 | grep "Allow: " | cut -b 8-20
+curl -I -sL -X OPTIONS -H "Allow: " $1
