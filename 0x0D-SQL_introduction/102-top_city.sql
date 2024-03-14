437d17cd64836ce47d8ed9c5caa1c8984fe1677a
greybeard <greybeard@gmail.com>
2024-03-15 00:33:15 +0300

    lll

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,7 @@
 --  displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(value) as avg_temp FROM hbtn_0c_0.temperatures WHERE month="July" OR month="August" GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
