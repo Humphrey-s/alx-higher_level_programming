d721705f8923ad6ad6e7a4c7a162f829b837f05a
greybeard <greybeard@gmail.com>
2024-06-04 14:20:45 +0300

    mm

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """LockedClass"""
 
-class LockedClass(object):
-    __slots__ = 'first_name'
+
+class LockedClass():
+    """allows class only one attribute"""
+    __slots__ = ('first_name')
+
+    def __init__(self):
+        self.first_name = 'first_name'
