2480542e6e00e90a1e4744e7dcd52990691493fd
greybeard <greybeard@gmail.com>
2024-06-04 23:45:17 +0300

    get maximum

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,2 @@
 --  displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
-SELECT city, AVG(value) as avg_temp
-FROM temperatures
-WHERE month = 7 OR month = 8
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
