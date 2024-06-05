a12f4e3508bec78d8010af969da919c6fb4adc76
greybeard <greybeard@gmail.com>
2024-06-05 12:06:07 +0300

    concatenate

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,19 +3,20 @@ const fs = require('fs');
 
 const fileA = process.argv[2];
 const fileB = process.argv[3];
+const fileC = process.argv[4];
 
 fs.readFile(fileA, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
     return;
   }
-  fs.appendFileSync('fileC', data);
+  fs.appendFileSync(fileC, data);
 
   fs.readFile(fileB, 'utf-8', function (err, data) {
     if (err) {
       console.log(err);
       return;
     }
-    fs.appendFileSync('fileC', data);
+    fs.appendFileSync(fileC, data);
   });
 });
