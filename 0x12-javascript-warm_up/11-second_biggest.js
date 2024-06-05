322271be6a13f34c03f5fec2798ed8499bc36292
greybeard <greybeard@gmail.com>
2024-06-05 08:42:44 +0300

    largest integer

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+let fArg = Number(argv[2]);
+
+if (isNaN(fArg) === true) {
+  console.log(0);
+  process.exit();
+}
+let i = 2;
+
+while (i < Number(argv.length)) {
+  if (fArg < Number(argv[i])) {
+    fArg = Number(argv[i]);
+  }
+  i++;
+}
+
+console.log(fArg);
