2e645d53afc18f8d0f40c8e05f28672cd9a2caeb
greybeard <greybeard@gmail.com>
2024-06-05 12:02:45 +0300

    concatenate 2 file

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+
+fs.readFile(fileA, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  fs.appendFileSync('fileC', data);
+
+  fs.readFile(fileB, 'utf-8', function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    fs.appendFileSync('fileC', data);
+  });
+});
